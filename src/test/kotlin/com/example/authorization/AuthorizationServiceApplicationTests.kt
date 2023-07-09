c6d3d7d19a045c8cdbabc422105b5f817b61254e
dianalora <diana.lora.a@gmail.com>
2023-07-09 14:34:21 +0200

    Add basic configuration for auth server

---
+++ src/test/kotlin/com/example/authorization/AuthorizationServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.authorization
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
